8c70420e663e0edc3da29c4366346ac113eb055d
Jack Merriman <113099332+jackmerriman@users.noreply.github.com>
2023-04-18 14:58:57 +0100

    Regression

--- Rcode/wrangling.R
+++ Rcode/wrangling.R
@@ -1,60 +1,68 @@
 library(tidyverse)
+
+#co-ordinates
+stations <- read.csv('2018_q3.csv') %>%
+  select(c(NAME, LATITUDE, LONGITUDE, BIKE.STANDS)) %>%
+  distinct()
+
+write.csv(stations, file = "stations.csv")
+
 Q3_2018 <- read.csv('2018_q3.csv') %>%
-  filter(NAME == "CHARLEMONT PLACE"|
-           NAME == "ST. STEPHEN'S GREEN EAST"|
-           NAME =="PHIBSBOROUGH ROAD")
+  filter(NAME == "PHIBSBOROUGH ROAD"|
+           NAME == "MERRION SQUARE SOUTH"|
+           NAME =="ROYAL HOSPITAL")
 Q4_2018 <- read.csv('2018_q4.csv') %>%
-  filter(NAME == "CHARLEMONT PLACE"|
-           NAME == "ST. STEPHEN'S GREEN EAST"|
-           NAME =="PHIBSBOROUGH ROAD")
+  filter(NAME == "PHIBSBOROUGH ROAD"|
+           NAME == "MERRION SQUARE SOUTH"|
+           NAME =="ROYAL HOSPITAL")
 Q1_2019 <- read.csv('2019_q1.csv') %>%
-  filter(NAME == "CHARLEMONT PLACE"|
-           NAME == "ST. STEPHEN'S GREEN EAST"|
-           NAME =="PHIBSBOROUGH ROAD")
+  filter(NAME == "PHIBSBOROUGH ROAD"|
+           NAME == "MERRION SQUARE SOUTH"|
+           NAME =="ROYAL HOSPITAL")
 Q2_2019 <- read.csv('2019_q2.csv') %>%
-  filter(NAME == "CHARLEMONT PLACE"|
-           NAME == "ST. STEPHEN'S GREEN EAST"|
-           NAME =="PHIBSBOROUGH ROAD")
+  filter(NAME == "PHIBSBOROUGH ROAD"|
+           NAME == "MERRION SQUARE SOUTH"|
+           NAME =="ROYAL HOSPITAL")
 Q3_2019 <- read.csv('2019_q3.csv') %>%
-  filter(NAME == "CHARLEMONT PLACE"|
-           NAME == "ST. STEPHEN'S GREEN EAST"|
-           NAME =="PHIBSBOROUGH ROAD")
+  filter(NAME == "PHIBSBOROUGH ROAD"|
+           NAME == "MERRION SQUARE SOUTH"|
+           NAME =="ROYAL HOSPITAL")
 Q4_2019 <- read.csv('2019_q4.csv') %>%
-  filter(NAME == "CHARLEMONT PLACE"|
-           NAME == "ST. STEPHEN'S GREEN EAST"|
-           NAME =="PHIBSBOROUGH ROAD")
+  filter(NAME == "PHIBSBOROUGH ROAD"|
+           NAME == "MERRION SQUARE SOUTH"|
+           NAME =="ROYAL HOSPITAL")
 Q1_2020 <- read.csv('2020_q1.csv') %>%
-  filter(NAME == "CHARLEMONT PLACE"|
-           NAME == "ST. STEPHEN'S GREEN EAST"|
-           NAME =="PHIBSBOROUGH ROAD")
+  filter(NAME == "PHIBSBOROUGH ROAD"|
+           NAME == "MERRION SQUARE SOUTH"|
+           NAME =="ROYAL HOSPITAL")
 Q2_2020 <- read.csv('2020_q2.csv') %>%
-  filter(NAME == "CHARLEMONT PLACE"|
-           NAME == "ST. STEPHEN'S GREEN EAST"|
-           NAME =="PHIBSBOROUGH ROAD")
+  filter(NAME == "PHIBSBOROUGH ROAD"|
+           NAME == "MERRION SQUARE SOUTH"|
+           NAME =="ROYAL HOSPITAL")
 Q3_2020 <- read.csv('2020_q3.csv') %>%
-  filter(NAME == "CHARLEMONT PLACE"|
-           NAME == "ST. STEPHEN'S GREEN EAST"|
-           NAME =="PHIBSBOROUGH ROAD")
+  filter(NAME == "PHIBSBOROUGH ROAD"|
+           NAME == "MERRION SQUARE SOUTH"|
+           NAME =="ROYAL HOSPITAL")
 Q4_2020 <- read.csv('2020_q4.csv') %>%
-  filter(NAME == "CHARLEMONT PLACE"|
-           NAME == "ST. STEPHEN'S GREEN EAST"|
-           NAME =="PHIBSBOROUGH ROAD")
+  filter(NAME == "PHIBSBOROUGH ROAD"|
+           NAME == "MERRION SQUARE SOUTH"|
+           NAME =="ROYAL HOSPITAL")
 Q1_2021 <- read.csv('2021_q1.csv') %>%
-  filter(NAME == "CHARLEMONT PLACE"|
-           NAME == "ST. STEPHEN'S GREEN EAST"|
-           NAME =="PHIBSBOROUGH ROAD")
+  filter(NAME == "PHIBSBOROUGH ROAD"|
+           NAME == "MERRION SQUARE SOUTH"|
+           NAME =="ROYAL HOSPITAL")
 Q2_2021 <- read.csv('2021_q2.csv') %>%
-  filter(NAME == "CHARLEMONT PLACE"|
-           NAME == "ST. STEPHEN'S GREEN EAST"|
-           NAME =="PHIBSBOROUGH ROAD")
+  filter(NAME == "PHIBSBOROUGH ROAD"|
+           NAME == "MERRION SQUARE SOUTH"|
+           NAME =="ROYAL HOSPITAL")
 Q3_2021 <- read.csv('2021_q3.csv') %>%
-  filter(NAME == "CHARLEMONT PLACE"|
-           NAME == "ST. STEPHEN'S GREEN EAST"|
-           NAME =="PHIBSBOROUGH ROAD")
+  filter(NAME == "PHIBSBOROUGH ROAD"|
+           NAME == "MERRION SQUARE SOUTH"|
+           NAME =="ROYAL HOSPITAL")
 Q4_2021 <- read.csv('2021_q4.csv') %>%
-  filter(NAME == "CHARLEMONT PLACE"|
-           NAME == "ST. STEPHEN'S GREEN EAST"|
-           NAME =="PHIBSBOROUGH ROAD")
+  filter(NAME == "PHIBSBOROUGH ROAD"|
+           NAME == "MERRION SQUARE SOUTH"|
+           NAME =="ROYAL HOSPITAL")
 
 
 
